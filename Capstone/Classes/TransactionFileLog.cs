134a39735f866caed11970a96310cbc6043cf84e
Amanda Arnold <arnoldamak@gmail.com>
2017-09-21 19:28:48 -0400

    Mandy added methods to Transaction File Log and cleaned up code in general

--- Capstone/Classes/TransactionFileLog.cs
+++ Capstone/Classes/TransactionFileLog.cs
@@ -8,5 +8,28 @@ namespace Capstone.Classes
 {
     class TransactionFileLog
     {
+
+        //This is the VM AUDIT WRITER from the whiteboard
+
+        public void RecordCompleteTransaction(decimal initialAmount)
+        {
+            
+        }
+
+        public void RecordDeposit(decimal depositAmount, decimal finalBalance)
+        {
+
+        }
+
+        public void RecordPurchase(string productName, string slotId, decimal initialBalance, decimal finalBalance)
+        {
+
+        }
+
+        public TransactionFileLog(string filepath)
+        {
+
+        }
+
     }
 }
