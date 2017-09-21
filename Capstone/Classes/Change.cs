878323d47ea9b1bd8e953f31049ed3c62867bbd2
Amanda Arnold <arnoldamak@gmail.com>
2017-09-21 14:35:59 -0400

    working on building inventory into dictionary

---
+++ Capstone/Classes/Change.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public class Change
+    {
+        private int dimes;
+        private int nickels;
+        private int quarters;
+        private double totalChange;
+
+        public int Dimes
+        {
+            get { return this.dimes; }
+        }
+
+        public int Nickels
+        {
+            get { return this.nickels; }
+
+        }
+
+        public int Quarters
+        {
+            get { return this.quarters; }
+        }
+
+        public double TotalChange
+        {
+            get { return this.totalChange; }
+        }
+
+        public Change(decimal amountInDollars, int amountInCents)
+        {
+           
+        }
+
+        public Change(decimal amountInDollars, int amountInCents)
+        {
+
+        }
+
+        ////We need to check into this one
+        //public void Equals(Change amountInDollars, Change amountInCents)
+        //{
+
+        //}
+
+    }
+}
---
+++ Capstone/Classes/InventoryFileDAL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    class InventoryFileDAL
+    {
+        private int Cost;
+        private string filepath;
+        private int InitialQuantity;
+        private int Product;
+        private int SlotId;
+
+        private int myVar;
+
+        public Dictionary<string, List<VendingMachineItem>> GetInventory()
+        {
+            filepath = Path.Combine(Directory.GetCurrentDirectory(), "vendingmachine.csv");
+            List<VendingMachineItem> vmList = new List<VendingMachineItem>();
+            Dictionary<string, List<VendingMachineItem>> vmDictionary = new Dictionary<string, List<VendingMachineItem>>();
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] slotArray = line.Split('|');
+
+                    ChipItem chipOne = new ChipItem(slotArray[1], Decimal.Parse(slotArray[2]));
+                    vmList.Add(chipOne);
+                    ChipItem chipTwo = new ChipItem(slotArray[1], Decimal.Parse(slotArray[2]));
+                    vmList.Add(chipTwo);
+                    ChipItem chipThree = new ChipItem(slotArray[1], Decimal.Parse(slotArray[2]));
+                    vmList.Add(chipThree);
+                    ChipItem chipFour = new ChipItem(slotArray[1], Decimal.Parse(slotArray[2]));
+                    vmList.Add(chipFour);
+                    ChipItem chipFive = new ChipItem(slotArray[1], Decimal.Parse(slotArray[2]));
+                    vmList.Add(chipFive);
+
+
+
+
+                    vmDictionary[slotArray[0]] = vmList;
+                }
+            }
+        }
+
+     
+
+
+
+
+
+    }
+}
