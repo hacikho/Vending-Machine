d2dfcf9c4beb5efea6a06c87133733ae414bb250
Haci Karahasanoglu <haci@hacikho.com>
2017-09-21 12:32:37 -0400

    Haci driver, completed Vending Machime Item, BeverageItem, ChipItem, CandyItem and GumItem classes

---
+++ Capstone/Classes/VendingMachineItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public abstract class VendingMachineItem
+    {
+        //Fields
+        private string itemName;
+        private decimal priceInCents;
+
+        //Properties
+        public string ItemName
+        {
+            get { return this.itemName; }
+        }
+
+        public decimal PriceInCents
+        {
+            get { return this.priceInCents; }
+        }
+
+        //constructor
+        public VendingMachineItem(string itemName, decimal price)
+        {
+            this.itemName = itemName;
+            this.priceInCents = price;
+        }
+
+
+        //abstract method
+        public abstract string Consume();
+
+    }
+}
---
+++ CapstoneTests/VendingMachineItemTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class VendingMachineItemTest
+    {
+        [TestMethod]
+        public void TetsCandyItem()
+        {
+            CandyItem skittles = new CandyItem("Skittles", 1.00M);
+            Assert.AreEqual("Munch Munch, Yum!", skittles.Consume());
+        }
+
+        [TestMethod]
+        public void TestBeverageItem()
+        {
+            BeverageItem pepsi = new BeverageItem("Diet pepsi", 2.00M);
+            Assert.AreEqual("Glug Glug, Yum!", pepsi.Consume());
+
+        }
+
+        [TestMethod]
+        public void TestChipItem()
+        {
+            ChipItem chip = new ChipItem("Lays", 2.00M);
+            Assert.AreEqual("Crunch Crunch, Yum!", chip.Consume());
+        }
+
+        [TestMethod]
+        public void TestGumItem()
+        {
+            GumItem gum = new GumItem("FreeDent", 4.00M);
+            Assert.AreEqual("Chew Chew, Yum!", gum.Consume());
+        }
+    }
+
+}
